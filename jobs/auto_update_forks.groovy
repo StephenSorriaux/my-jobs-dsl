286bd06fbf5e018022d0c0a63c8207ef36772b08
Stephen <stephen.sorriaux@gmail.com>
2018-10-02 19:30:48 +0200

    Add jobs for auto-update forks

---
+++ jobs/auto_update_forks.groovy
@@ -0,0 +1,39 @@
+def myForks = [
+  'ansible': [
+    ownerAndProject: 'ansible/ansible',
+    branch: 'devel'],
+  'postgres-operator': [
+    ownerAndProject: 'CrunchyData/postgres-operator',
+    branch: 'develop']]
+
+myForks.keySet().each{ repo ->
+
+  this.freeStyleJob("auto-update-${repo}-fork") {
+      logRotator(-1, 10)
+      wrappers {
+        credentialsBinding {
+          sshAgent('github-ssh')
+          environmentVariables(BRANCH: myForks[repo]['branch'], PROJECT: repo)
+        }
+      }
+      scm {
+        git {
+          remote {
+            github("${myForks[repo]['ownerAndProject']}")
+            credentials('github')
+          }
+          branches("${myForks[repo]['branch']}")
+          extensions {
+            cleanBeforeCheckout()
+          }
+        }
+      }
+      triggers {
+          scm('H/5 * * * *')
+      }
+      steps {
+          shell(readFileFromWorkspace('scripts/update_fork.sh'))
+      }
+  }
+
+}
---
+++ scripts/update_fork.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -ev
+if [ ! -n "$(grep "^github.com " ~/.ssh/known_hosts)" ]; then
+  ssh-keyscan github.com >> ~/.ssh/known_hosts 2>/dev/null
+fi
+fork_exists=$(git remote | grep fork | wc -l)
+if [ "$fork_exists" -eq 0 ]; then
+  git remote add fork git@github.com:StephenSorriaux/${PROJECT}.git
+fi
+git push -f fork HEAD:${BRANCH}
