f227c7be02bc6473cab59735737779de8c20c299
Stephen <stephen.sorriaux@gmail.com>
2018-10-18 12:01:52 +0200

    Only one script for all mangos jobs

--- jobs/auto_create_mangos_docker_images.groovy
+++ jobs/auto_create_mangos_docker_images.groovy
@@ -2,6 +2,25 @@ def repoList = ['mangosone', 'mangostwo', 'mangosthree']
 
 repoList.each{ repo ->
 
+  this.freeStyleJob("auto-create-${repo}-docker-database-image") {
+      logRotator(-1, 10)
+      wrappers {
+        credentialsBinding {
+          usernamePassword('USER', 'PASSWORD', 'docker-hub')
+          environmentVariables(PROJECT: repo)
+        }
+      }
+      scm {
+          github("${repo}/database", 'master')
+      }
+      triggers {
+          scm('H/5 * * * *')
+      }
+      steps {
+          shell(readFileFromWorkspace('scripts/build_mangos_database.sh'))
+      }
+  }
+
   this.freeStyleJob("auto-create-${repo}-docker-server-image") {
       logRotator(-1, 10)
       wrappers {
@@ -18,6 +37,25 @@ repoList.each{ repo ->
       }
       steps {
           shell(readFileFromWorkspace('scripts/build_mangos.sh'))
+      }
+  }
+
+  this.freeStyleJob("auto-create-${repo}-docker-realmd-image") {
+      logRotator(-1, 10)
+      wrappers {
+        credentialsBinding {
+          usernamePassword('USER', 'PASSWORD', 'docker-hub')
+          environmentVariables(PROJECT: repo)
+        }
+      }
+      scm {
+          github("${repo}/server", 'refs/tags/*')
+      }
+      triggers {
+          scm('H/5 * * * *')
+      }
+      steps {
+          shell(readFileFromWorkspace('scripts/build_mangos_realmd.sh'))
       }
   }
 
