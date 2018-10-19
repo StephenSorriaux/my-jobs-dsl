cf75a01db6679b5fddf8076b0cddda7e1d681d52
Stephen <stephen.sorriaux@gmail.com>
2018-10-19 18:07:58 +0200

    Checking if known_hosts exists

--- scripts/update_fork.sh
+++ scripts/update_fork.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 set -ev
+if [ ! -d "${HOME}/.ssh" ]; then
+  mkdir ~/.ssh
+fi
+if [ ! -f "${HOME}/.ssh/known_hosts" ]; then
+  touch ~/.ssh/known_hosts
+fi
 if [ ! -n "$(grep "^github.com " ~/.ssh/known_hosts)" ]; then
   ssh-keyscan github.com >> ~/.ssh/known_hosts 2>/dev/null
 fi
